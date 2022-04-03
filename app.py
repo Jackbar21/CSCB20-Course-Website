1e145bff6a17edf5288469943083a6dafe608213
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-04-03 16:08:08 -0400

    Added basic html and css pages (to test template inheritance)

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Welcome to A3!"
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
