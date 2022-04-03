e9fc27fc0403fb6066b4dc0ffcceb13007501791
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-04-03 19:58:51 -0400

    finished message flashing learning

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -15,10 +15,12 @@ def login():
         session.permanent = True
         user = request.form["name_KEY"]
         session["user"] = user
+        flash("Login Successful!", "info")
         return redirect(url_for("user"))
 
     
     if "user" in session:
+        flash("Already logged in!", "info")
         return redirect(url_for("user"))
     
     return render_template("login.html")
@@ -28,12 +30,14 @@ def login():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return render_template("user.html", user=user)
     
+    flash("You are not logged in!", "info")
     return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    flash("You have been logged out successfully!", "info")
     session.pop("user", None)
     return redirect(url_for("login"))
 
