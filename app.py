852d9d347bfac4c2ab6a6eb661d6194372b5dde1
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-04-03 19:45:03 -0400

    finishing session code

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "SECRET_KEY"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
@@ -9,15 +12,30 @@ def home():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        session.permanent = True
         user = request.form["name_KEY"]
-        return redirect(url_for("user", usr=user))
-    # else condition
+        session["user"] = user
+        return redirect(url_for("user"))
+
+    
+    if "user" in session:
+        return redirect(url_for("user"))
+    
     return render_template("login.html")
     
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    
+    return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     app.run(debug=True)
