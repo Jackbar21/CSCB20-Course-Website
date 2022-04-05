5626fb992f207b1baa10758a66d27e2cb6a23f5c
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-04-04 23:00:59 -0400

    added proper secret_key

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from secrets import token_hex
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = token_hex(16) # from secrets import
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
