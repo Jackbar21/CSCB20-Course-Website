293da4562cf3ccab45d8ab9cf081cf4b151359a3
Alejandro Iglesias Llobet <alejandro.iglesiasllobet3@gmail.com>
2022-04-05 00:03:00 -0400

    set up Purva's starter code

--- app.py
+++ app.py
@@ -26,9 +26,90 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}', '{self.name}', '{self.email}', '{'Instructor' if self.is_instructor else 'Student'}')"
 
-@app.route("/")
+@app.route('/')
+@app.route('/home')
 def home():
-    return "Welcome to A3!"
+    pagename = 'home'
+    return render_template('home.html', pagename = pagename)
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        username = request.form['Username']
+        email = request.form['Email']
+        hashed_password = bcrypt.generate_password_hash(request.form['Password']).decode('utf-8')
+        reg_details =(
+            username,
+            email,
+            hashed_password
+        )
+        add_users(reg_details)
+        flash('Registration Successful! Please login now:')
+        return redirect(url_for('login'))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        if 'name' in session:
+            flash('already logged in!!')
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html')
+    else:
+        username = request.form['Username']
+        password = request.form['Password']
+        person = Person.query.filter_by(username = username).first()
+        if not person or not bcrypt.check_password_hash(person.password, password):
+            flash('Please check your login details and try again', 'error')
+            return render_template('login.html')
+        else:
+            session['name'] = username
+            session.permanent = True
+            return redirect(url_for('home'))
+
+@app.route('/notes', methods = ['GET', 'POST'])
+def notes():
+    if request.method == 'GET':
+        query_notes_result = query_notes()
+        return render_template('notes.html', query_notes_result = query_notes_result)
+
+@app.route('/add', methods = ['GET', 'POST'])
+def add():
+    if request.method == 'GET':
+        return render_template('add.html')
+    else:
+        note_details =(
+            request.form['Note_ID'],
+            request.form['Title'],
+            request.form['Content'],
+            request.form['Your_ID']
+        )
+        add_notes(note_details)
+        return render_template('add_success.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('name', default = None)
+    return redirect(url_for('home'))
+
+def query_notes():
+    query_notes = Notes.query.all()
+    return query_notes
+
+def add_notes(note_details):
+    note = Notes(id = note_details[0], title = note_details[1], content = note_details[2], person_id = note_details[3])
+    db.session.add(note)
+    db.session.commit()
+
+def add_users(reg_details):
+    person = Person(username = reg_details[0], email = reg_details[1], password = reg_details[2])
+    db.session.add(person)
+    db.session.commit()
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
